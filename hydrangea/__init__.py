1a08d22c81d99462ab9705b1c47de5d0ab130716
bahe@strw.leidenuniv.nl <bahe@strw.leidenuniv.nl>
2020-01-15 18:08:35 +0100

    Changed import order

--- hydrangea/__init__.py
+++ hydrangea/__init__.py
@@ -1,8 +1,7 @@
-from hydrangea.read_region import ReadRegion
-from hydrangea.split_file import SplitFile
 import hydrangea.hdf5
-from hydrangea.tools import *
 import hydrangea.crossref
 
-# TEMPORARY, for compatibility
-from hydrangea.tools import get_snepshot_indices
+from hydrangea.read_region import ReadRegion
+from hydrangea.split_file import SplitFile
+
+from hydrangea.tools import *
