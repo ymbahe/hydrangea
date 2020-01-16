ac80024b9cced6496c92c40a4e1b1eb499ddb833
Yannick Bahe <bahe@strw.leidenuniv.nl>
2020-01-16 16:36:03 +0100

    Started Cantor object

--- hydrangea/objects.py
+++ hydrangea/objects.py
@@ -3,6 +3,7 @@
 """Classes for efficient working with simulations and snapshots."""
 
 from pdb import set_trace
+import os
 
 import hydrangea as hy
 
@@ -46,3 +47,57 @@ class Simulation:
     def get_subfind_file(self, index):
         """Form the (first) file of a given subfind catalogue."""
         return hy.form_files(self.run_dir, index, types='sub')
+
+
+class Cantor:
+    """Representation of a Cantor catalogue."""
+
+    def __init__(self, sim, index=None, snap_type='snap'):
+        """Object constructor.
+
+        Parameters
+        ----------
+        sim : Simulation instance
+            The simulation object to which this Cantor catalogue belongs.
+        index : int, optional
+            The output index of this catalogue. If None (default), the
+            object refers to global quantities.
+        snap_type : str, optional
+            Whether this Cantor catalogue belongs to a snapshot
+            ('snap', default), or snipshot ('snip').
+        """
+        # Set up paths to catalogue files
+        if index is not None:
+            self.catloc = sim.cantor_dir + f'Cantor_{index}.hdf5'
+            if not os.path.isfile(self.catloc):
+                print(f"Cantor catalogue {index} not found!")
+                self.catloc = None
+                return
+            self.idloc = sim.cantor_dir + f'Cantor_{index}_IDs.hdf5'
+            if not os.path.isfile(self.idloc):
+                self.idloc = None
+            self.radloc = sim.cantor_dir + f'Cantor_{index}_Radii.hdf5'
+            if not os.path.isfile(self.radloc):
+                self.radloc = None
+        else:
+            print("Setting up cross-snapshot functionality is still "
+                  "in progress. Please come back later...")
+            set_trace()
+
+    def __getattr__(self, name):
+        """Provide a way to handle requests for new attributes."""
+        if name == 'IDs':
+            if self.idloc is None:
+                print("IDs file not found -- cannot load!")
+                set_trace()
+            setattr(self, name, hy.hdf5.read_data(self.idloc, name))
+        elif name == 'Radii':
+            if self.radloc is None:
+                print("Radius file not found -- cannot load!")
+                set_trace()
+            setattr(self, name, hy.hdf5.read_data(self.radloc, name))
+        else:
+            data = hy.hdf5.read_data(self.catloc, name.replace('__', '/'))
+            if data is None:
+                print(f"Could not find requested data '{name}'")
+            setattr(self, name, data)
