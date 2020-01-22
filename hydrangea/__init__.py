2eded208a9056d02e65c804eaad285ef023da122
bahe@strw.leidenuniv.nl <bahe@strw.leidenuniv.nl>
2020-01-22 12:25:10 +0100

    Added units.py to __init__

--- hydrangea/__init__.py
+++ hydrangea/__init__.py
@@ -1,7 +1,16 @@
+"""
+import hdf5
+import crossref
+import objects
+import download
+import units
+"""
+
 import hydrangea.hdf5
 import hydrangea.crossref
 import hydrangea.objects
 import hydrangea.download
+import hydrangea.units
 
 from hydrangea.read_region import ReadRegion
 from hydrangea.split_file import SplitFile
