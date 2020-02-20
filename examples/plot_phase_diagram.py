e3bb99d525c8c1fb84d1b05118328eeb3924c287
Yannick Bahe <bahe@strw.leidenuniv.nl>
2020-02-20 16:33:55 +0100

    Updated plot_phase_diagram example

--- examples/plot_phase_diagram.py
+++ examples/plot_phase_diagram.py
@@ -7,10 +7,15 @@ import matplotlib.pyplot as plt
 # Set script parameters
 sim_index = 14                 # Which simulation do we want?
 snap_index = 29                # Which snapshot do we want to analyse?
+temp_range = [3.0, 9.0]        # Plot range in (log) temperature [K]
+nH_range = [-9.0, 3.0]         # Plot range in (log) nH [cm^-3]
+nbins = 100                    # Number of bins per axis
 plotloc = 'phase_diagram.png'  # Where to save the output plot?
 
 # Prepare the plot
 fig = plt.figure(figsize=(5, 4))
+fig = plt.figure(figsize=(5/0.8, 4))
+ax1 = fig.add_axes([0.15, 0.15, 0.65, 0.8])
 
 # Set up the simulation object for the run we're working with, to easily
 # get the relevant file paths
@@ -32,31 +37,43 @@ fof_cl = hy.SplitFile(subfind_file, 'FOF', read_index=0)
 gas = hy.SplitFile(snapshot_file, part_type=0)
 
 # Get all particles within 10r200 from the cluster centre
-# (i.e. in the well-resolved region)
+# (i.e. in the well-resolved region). Note that we could also have set up
+# a ReadRegion around the cluster centre, as demonstrated in the example
+# 'find_particles_near_subhalo.py' to read particles within this sphere.
 ind_select = np.nonzero(
     np.linalg.norm(gas.Coordinates - fof_cl.GroupCentreOfPotential[None, :],
                    axis=1) <= 10.0*fof_cl.Group_R_Crit200)[0]
 
-# Create the 2D histogram
+# Create the 2D histogram. The particle temperatures, densities, and 
+# masses are read in implicitly as they are being accessed. 
 histogram, xe, ye = np.histogram2d(np.log10(gas.Temperature[ind_select]),
                                    np.log10(gas.Density[ind_select]),
                                    weights=gas.Mass[ind_select],
-                                   bins=[50, 50], range=[[3, 9], [-6, 3]])
+                                   bins=[nbins, nbins], 
+                                   range=[temp_range, nH_range])
 
-da = (xe[1]-xe[0]) * (ye[1]-ye[0])
 
 # Plot the 2D histogram
-plt.imshow(np.log10(histogram/da), cmap=plt.cm.inferno,
-           extent=[-6, 3, 3, 9], origin='lower')
+da = (xe[1]-xe[0]) * (ye[1]-ye[0])
+im = plt.imshow(np.log10(histogram/da), cmap=plt.cm.inferno,
+           extent=[-6, 3, 3, 9], origin='lower', aspect='equal')
 
 ax = plt.gca()
-ax.set_xlim((-6, 3))
-ax.set_ylim((3, 9))
+ax.set_xlim(nH_range)
+ax.set_ylim(temp_range)
 ax.set_xlabel(r'$\log_{10}\,(n_\mathrm{H}\,[\mathrm{cm}^{-3}])$')
 ax.set_ylabel(r'$\log_{10}\,(T\,[\mathrm{K}])$')
 
+# Add a colour bar on the right side
+ax2 = fig.add_axes([0.81, 0.15, 0.05, 0.8])
+ax2.set_xticks([])
+ax2.set_yticks([])
+cbar = plt.colorbar(im, cax=ax2, orientation='vertical')
+fig.text(0.95, 0.5, r'log$_{10}$ (d$M_\mathrm{gas}$ / ' 
+         '(dlog$_{10}\,T$ dlog$_{10}\,n_\mathrm{H}$) [M$_\odot$])',
+         rotation=90.0, va='center', ha='left')
+
 # Save the figure
-plt.subplots_adjust(left=0.15, bottom=0.15, right=0.95, top=0.95)
 plt.show
 plt.savefig(plotloc, transparent=False, dpi=150)
                         
