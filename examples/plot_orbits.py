0d5fb1b47f437ebedaea453f4ca9e9161b690ced
Yannick Bahe <bahe@strw.leidenuniv.nl>
2020-02-23 18:29:17 +0100

    Finished plot_orbits.py example

--- examples/plot_orbits.py
+++ examples/plot_orbits.py
@@ -1,9 +1,16 @@
-"""Demonstration script to plot the orbit of one galaxy in HYDRO and DM."""
+"""Demonstration script to plot the orbit of one galaxy in HYDRO and DM.
+
+It demonstrates reading data from the pre-compiled galaxy tables, matching
+subhaloes between the HYDRO and DM-ONLY versions of a simulation, and 
+working with the interpolated high-time-resolution orbit tables.
+"""
 
 # Import required packages
 import numpy as np
 import hydrangea as hy
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors   # For truncated grey-scale color map
+from pdb import set_trace
 
 # Set script parameters
 sim_index = 0                  # Which simulation do we want?
@@ -16,8 +23,8 @@ sim_hydro = hy.Simulation(index=sim_index)
 sim_dm_only = hy.Simulation(index=sim_index, sim_type='DM')
 
 # Prepare the plot
-fig = plt.figure(figsize=(4, 4))
-
+fig = plt.figure(figsize=(4*0.8/0.6, 4))
+ax1 = fig.add_axes((0.15, 0.15, 0.65, 0.8))
 
 # ------------------------------------------------------------------
 # Part I: Find the corresponding galaxy ID in the DM-only simulation
@@ -62,7 +69,7 @@ central_id_dmo = hy.hdf5.read_data(sim_dm_only.fgt_loc, 'CenGal',
 def get_galaxy_paths(galaxy_id, sim, load_times=False):
     """Extract the galaxy paths for a specified galaxy in a simulation."""
     # First, we must look up the galaxy in the interpolation list
-    interp_id = hy.hdf5.read_data(sim.interp_loc, 'GalaxyRevIndex',
+    interp_id = hy.hdf5.read_data(sim.interpolation_loc, 'GalaxyRevIndex',
                                   read_index=galaxy_id)
 
     # If the galaxy is not in the interpolation list, quit
@@ -71,14 +78,16 @@ def get_galaxy_paths(galaxy_id, sim, load_times=False):
         return
 
     # Extract the interpolated orbit
-    orbit_pos = hy.hdf5.read_data(sim.interp_loc, 'InterpolatedPositions',
+    orbit_pos = hy.hdf5.read_data(sim.interpolation_loc,
+                                  'InterpolatedPositions',
                                   read_index=interp_id)
 
     # If needed, also load the interpolation times
     if not load_times:
         return orbit_pos
 
-    orbit_times = hy.hdf5.read_data(sim.interp_loc, 'InterpolationTimes')
+    orbit_times = hy.hdf5.read_data(sim.interpolation_loc,
+                                    'InterpolationTimes')
     return orbit_pos, orbit_times
 
 
@@ -93,23 +102,40 @@ orbit_dmo = (get_galaxy_paths(galaxy_id_dmo, sim_dm_only)
 # Part III: Plot the orbits
 # -------------------------
 
-plt.plot(orbit_hydro[0, :], orbit_hydro[1, :], color='green', linewidth=0.5)
-plt.scatter(orbit_hydro[0, :], orbit_hydro[1, :], c=orbit_times,
-            edgecolor='none', vmin=0, vmax=14.0, cmap=plt.cm.viridis)
+# Create a truncated grey color map (Greys_r becomes white towards 1)
+# [see https://stackoverflow.com/questions/18926031/]
+cmap_greys = colors.LinearSegmentedColormap.from_list(
+    'trunc(Greys_r, 0.0, 0.8)', plt.cm.Greys_r(np.linspace(0.0, 0.8, 1000)))
 
-plt.plot(orbit_dmo[0, :], orbit_dmo[1, :], color='black', linewidth=0.5)
-plt.scatter(orbit_dmo[0, :], orbit_dmo[1, :], c=orbit_times,
-            edgecolor='none', vmin=0, vmax=14.0, cmap=plt.cm.Greys)
-
+plt.sca(ax1)
+sc_dm = plt.scatter(orbit_dmo[0, :], orbit_dmo[1, :], c=orbit_times,
+                    edgecolor='none', vmin=0, vmax=14.0, cmap=cmap_greys,
+                    s=10)
+sc_hy = plt.scatter(orbit_hydro[0, :], orbit_hydro[1, :], c=orbit_times,
+                    edgecolor='none', vmin=0, vmax=14.0, cmap=plt.cm.viridis,
+                    s=10)
 
 # Some embellishments on the plot
-ax = plt.gca()
-ax.set_xlim((-1, 1))
-ax.set_ylim((-1, 1))
-ax.set_xlabel(r'$\Delta x$ [Mpc]')
-ax.set_ylabel(r'$\Delta y$ [Mpc]')
+ax1.set_xlim((-0.8, 0.8))
+ax1.set_ylim((-0.8, 0.8))
+ax1.set_xlabel(r'$\Delta x$ [pMpc]')
+ax1.set_ylabel(r'$\Delta y$ [pMpc]')
+
+# Add colour bars on the side
+ax2 = fig.add_axes([0.81, 0.15, 0.03, 0.8])
+ax2.set_xticks([])
+ax2.set_yticks([])
+cbar_dm = plt.colorbar(sc_dm, cax=ax2, orientation='vertical')
+cbar_dm.set_ticks([])
+
+ax3 = fig.add_axes([0.84, 0.15, 0.03, 0.8])
+ax3.set_xticks([])
+ax3.set_yticks([])
+cbar_hy = plt.colorbar(sc_hy, cax=ax3, orientation='vertical')
+
+fig.text(0.94, 0.55, 'Age of the Universe [Gyr]',
+         rotation=90.0, va='center', ha='left')
 
 # Save the figure
-plt.subplots_adjust(left=0.14, bottom=0.15, right=0.82, top=0.88)
 plt.show
 plt.savefig(plotloc, transparent=False, dpi=150)
